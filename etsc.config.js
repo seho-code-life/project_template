92d34d574ac7d677fa93eb13ae5b84590391e96f
seho <1018715564@qq.com>
2021-09-02 09:50:54 +0800

    ✨ feat(all): first commit

---
+++ etsc.config.js
@@ -0,0 +1,15 @@
+const esbuildPluginTsc = require('esbuild-plugin-tsc');
+
+module.exports = {
+  outDir: './dist',
+  esbuild: {
+    minify: false,
+    target: 'es2015',
+    plugins: [esbuildPluginTsc()]
+  },
+  assets: {
+    baseDir: 'src',
+    outDir: './dist',
+    filePatterns: ['**/*.json']
+  }
+};
