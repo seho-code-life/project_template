1245d602546d2507194f236a85c994f5b31573e7
seho <1018715564@qq.com>
2021-09-03 17:56:27 +0800

    🐞 fix: 修复已知依赖问题

--- etsc.config.js
+++ etsc.config.js
@@ -1,15 +1,5 @@
-const esbuildPluginTsc = require('esbuild-plugin-tsc');
-
 module.exports = {
-  outDir: './dist',
   esbuild: {
-    minify: false,
-    target: 'es2016',
-    plugins: [esbuildPluginTsc()]
-  },
-  assets: {
-    baseDir: 'src',
-    outDir: './dist',
-    filePatterns: ['**/*.json']
+    minify: true
   }
 };
