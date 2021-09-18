2d6ccac9e613721bfbd1e410cfdf911e92eb48b3
seho <1018715564@qq.com>
2021-09-18 14:31:49 +0800

    🐞 fix: 添加默认布局模板

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="layout-content">
     <RouterView></RouterView>
-  </div>
 </template>
 <script lang="ts" setup>
 import { RouterView } from "vue-router"
 </script>
-<style lang="scss"></style>
