4df5158407bf2937c9f95bab690243844b1a2eaf
seho <1018715564@qq.com>
2021-10-29 14:03:40 +0800

    🧪 test: 修改测试用例

--- tests/model/api/list.test.ts
+++ tests/model/api/list.test.ts
@@ -3,13 +3,13 @@ import ListController from '../../../src/controller/list'
 describe('addList', () => {
   const controller = new ListController()
   it('添加参数测试-1', async () => {
-    await controller.addList({
+    await controller.createList({
       title: 'test',
       content: 'nihao'
     })
   })
   it('添加参数测试-2', async () => {
-    const result = await controller.addList({
+    const result = await controller.createList({
       title: 'test2',
       content: 'hello?'
     })
