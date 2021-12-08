95972a0e098aa8076387c1be6f0721906ee7b889
seho <1018715564@qq.com>
2021-12-08 16:17:22 +0800

    🐞 fix: 完善lint校验ts的逻辑 #3

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{ts,tsx}': ['prettier --write', 'eslint --fix', () => 'tsc --noEmit --skipLibCheck'],
-  '*.vue': ['prettier --write', 'eslint --fix', () => 'vue-tsc --noEmit --skipLibCheck'],
-  '*.{json,js,jsx}': ['prettier --write', 'eslint --fix']
-}
