8663fbf6274411842dd1d321ed3f93d43731965e
seho <1018715564@qq.com>
2021-12-08 15:17:53 +0800

    🎈 perf: 优化lint-staged命令 #3

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts,tsx}': ['prettier --write', 'eslint --fix', 'tsc --noEmit'],
-  '*.vue': ['prettier --write', 'eslint --fix', 'vue-tsc --noEmit'],
+  '*.{ts,tsx}': ['prettier --write', 'eslint --fix', () => 'tsc --noEmit --skipLibCheck'],
+  '*.vue': ['prettier --write', 'eslint --fix', () => 'vue-tsc --noEmit --skipLibCheck'],
   '*.{json,js,jsx}': ['prettier --write', 'eslint --fix']
 }
